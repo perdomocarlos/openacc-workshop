e98b436124f8f734b66eb3c0a14fabdd16002c65
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2017-08-07 19:42:22 -0500

    Kernels example

--- 1.kernels/vector.c
+++ 1.kernels/vector.c
@@ -1,33 +1,62 @@
 #include <stdio.h>      /* Standard I/O Library: printf */
 #include <stdlib.h>     /* Standard Library: malloc, calloc, free, ralloc */
 #include <string.h>     /* String funtions: strcpy*/
-#include <time.h>
 
-#define MATRIX_SIZE 10000000
+#define N 10000000
 
-int main() {
-    
-    /* Requesting dynamic memory for space to allocate array 
-    arrays are created commonly like array[MATRIX_SIZE]. However
-    when MATRIX_SIZE is too large it is neccesary to create it
-    as described as follows.
+/* Creating an alias for unsigned long int datatype */
+typedef unsigned long int long_int;
+
+int main(){
+    /* Requesting dynamic memory for space to allocate A,B,C 
+    Arrays are created commonly like A[N], B[N], C[N]. However
+    when N is too large it is neccesary to create them in the way
+    described as follows.
     */
-    int * array = (int *) malloc( sizeof(int) * MATRIX_SIZE );
+    long_int * A = (long_int *) malloc( N * sizeof(long_int) );
+    long_int * B = (long_int *) malloc( N * sizeof(long_int) );
+    long_int * C = (long_int *) malloc( N * sizeof(long_int) );
+    long_int * D = (long_int *) malloc( N * sizeof(long_int) );
+
+
+    /* Initializing A, B, C */
+    for(int i=0; i<N; ++i){ 
+        A[i] = 2;
+        B[i] = 2;
+        C[i] = 0;
+        D[i] = 0;
+
+    }
 
     #pragma acc kernels
-    for(int i = 0; i < MATRIX_SIZE; i++) array[i] = 1;
+    {
+        /* Vector sum  */
+        for(int j=0; j<N; ++j) C[j] = A[j] + B[j];
+
+        /* Vector multiplicaion */
+        for(int j=0; j<N; ++j) D[j] = A[j] * B[j];
+    }
+
 
     /* Checking the answer */
     char validation[10];
     strcpy(validation,"Ok");
-    for(int i=0;i<MATRIX_SIZE;i++)
-        if( array[i] != 1) strcpy(validation, "Fail");
+    for(int k=0; k<N; ++k) 
+        if (C[k] != 4) strcpy(validation,"Fail");
 
-    printf ("\n");
-    printf ("Validation: %s \n", validation);
-    printf ("Vector size: %d \n", MATRIX_SIZE);
+    for(int k=0; k<N; ++k) 
+        if (D[k] != 4) strcpy(validation,"Fail");
 
-    free(array);
+    printf ( "\nSize %d, Validation %s\n", N, validation );
+
+    /* When we request dynamic memory for large arrays
+    it is neccesary to release it before the application finish.
+    If you do not do this, the memory will be occupied with the data
+    of the arrays A, B and C regardless of whether the program is 
+    finished. */
+    free(A);
+    free(B);
+    free(C);
 
     return EXIT_SUCCESS;
 }
